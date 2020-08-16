810270420d07c1e441a42f1a673cbbc839b04dfd
yasirkhan-4925 <64003803+yasirkhan-4925@users.noreply.github.com>
2020-08-16 17:29:12 +0500

    with firebase

--- app.js
+++ app.js
@@ -1,48 +1,81 @@
 
 var todoitems=document.getElementById("todoitems");
 function addtodo(){
-    var todo=document.getElementById("todo-input")
+    var todotext=document.getElementById("todo-input")
+    
+    var key=firebase.database().ref('todos').push().key;
+    
+     
+    var todo={
+         value:todotext.value,
+         key:key
+    }
+
+    firebase.database().ref('todos').child(key).set(todo)
+
+    todotext.value="";
+    
+}
+
+
+
+
+function deleteall()
+{
+     todoitems.innerHTML="";
+}
+
+
+
+// extracting data from firebase
+
+firebase.database().ref('todos').on('child_added',function(data){
+ 
     var list=document.createElement("li");
-    var list_text=document.createTextNode(todo.value);
+    var list_text=document.createTextNode(data.val().value);
     list.appendChild(list_text);
     todoitems.appendChild(list);
-    todo.value="";
+   
     var delbtn=document.createElement("button");
     var delbtn_text= document.createTextNode("Delete");
     delbtn.appendChild(delbtn_text);
     delbtn.setAttribute("class","btn btn-danger flt ");
-    delbtn.setAttribute("onclick","tododel(this)")
-    list.appendChild(delbtn);
-    
-
-    //create edit button 
+    delbtn.setAttribute("onclick","tododel(this)");
+    delbtn.setAttribute("id",data.val().key)
+  
 
     var editbtn=document.createElement("button");
     var editbtn_text=document.createTextNode("Edit");
     editbtn.appendChild(editbtn_text);
-    list.appendChild(editbtn);
+
     editbtn.setAttribute("class","btn btn-dark flt");
     editbtn.setAttribute("onclick","edittodo(this)");
+    editbtn.setAttribute("id",data.val().key)
+    list.appendChild(delbtn);
+    list.appendChild(editbtn);
    
+     
+})
 
-    
-   
-
-    
-}
 
 function tododel(e){
-  e.parentNode.remove();
-}
+    firebase.database().ref('todos').child(e.id).remove();
+    e.parentNode.remove();
+   }
 
+
+
+
+   
 function edittodo(e)
 {
      var editText=e.parentNode.firstChild.nodeValue;
      var editValue=prompt("Enter Edit Value",editText);
      e.parentNode.firstChild.nodeValue=editValue;
-}
 
-function deleteall()
-{
-     todoitems.innerHTML="";
+     var todoedit={
+          value:editValue,
+          key:e.id
+     }
+     firebase.database().ref('todos').child(e.id).set(todoedit)
 }
